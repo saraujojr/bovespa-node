591e21401d2f8e7e777a4a2638ef73c0617537b0
Silvio Araujo <saraujojr@gmail.com>
2017-10-06 03:57:35 -0300

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    url: "http://www.bmfbovespa.com.br/pesquisapregao/download?filelist=",
+    priceReport: {
+      namespace: "urn:bvmf.217.01.xsd"
+    }
+}
---
+++ index.js
@@ -0,0 +1,109 @@
+const fs = require('fs'),
+     download = require('download'),
+     _ = require('lodash'),
+     dateformat = require('dateformat'),
+     fstream = require('fstream'),
+     decompress = require('decompress'),
+     xpath = require('xpath'),
+     dom = require('xmldom').DOMParser,
+     xmljs = require('xml-js');
+
+var getConfig = () => require('./config/config.js').url;
+
+var getPriceReportNodes = (xmlDoc) => {
+
+  //console.log('Testing...');
+  var priceReports = new Array();
+  var doc = new dom().parseFromString(xmlDoc)
+
+  var namespace = require('./config/config.js').priceReport.namespace;
+  var nodes = xpath.select(`//*[local-name(.)='Document' and namespace-uri(.)='${namespace}']`, doc);
+
+  for (var i=0; i < nodes.length; i++){
+    if (xmljs.xml2js(nodes[i].toString(), {compact: true}).Document.PricRpt) {
+      priceReports.push(xmljs.xml2js(nodes[i].toString(), {compact: true}).Document.PricRpt);
+    }
+  }
+
+  return priceReports;
+
+}
+
+module.exports.getPriceReport = (date) => {
+
+  return new Promise((resolve, reject) => {
+
+    try{
+
+      var url = getConfig();
+
+      if (_.isDate(date)) {
+
+          var fileToDownload = 'PR' + dateformat(date, "yymmdd") + '.zip';
+
+          url = url + fileToDownload;
+          console.log('Downloading ' + url + '...');
+
+          download(url).then((data) => {
+            fs.writeFileSync('temp/download.zip', data);
+
+            console.log('Unzip files...');
+
+            decompress('temp/download.zip', 'temp').then((files) => {
+              return decompress('temp/' + fileToDownload, 'temp');
+            }, (err) => {
+              reject({
+                status: 'ERROR',
+                errorMessage: 'Unable to unzip the file',
+                errorObject: err
+              });
+            }).then((files) => {
+
+              var reportsArray = new Array();
+              for(var i=0;i < files.length; i++){
+
+                var report = new Object();
+                report.fileName = files[i].path;
+                report.priceReports = getPriceReportNodes(files[0].data.toString('utf-8'));
+                reportsArray.push(report);
+              }
+
+              resolve({
+                  status: 'OK',
+                  errorMessage: '',
+                  path: 'temp/' + fileToDownload,
+                  reports: reportsArray
+                });
+
+            }, (err) => {
+              reject({
+                status: 'ERROR',
+                errorMessage: 'Unable to unzip the file',
+                errorObject: err
+              });
+            });
+
+          }, (err) => {
+            console.log(err);
+            reject({
+              status: 'ERROR',
+              errorMessage: err.statusCode + ':' + err.statusMessage
+            });
+          });
+
+      }else{
+          reject ({
+            status: 'ERROR',
+            errorMessage: `Invalid Date: ${date}.`
+          });
+      }
+
+    }catch (e){
+      reject({
+        status: 'ERROR',
+        errorMessage: e.message
+      });
+    }
+  });
+
+}
